b74d709264fa3d989ccd8542ed489c7b36a1bae2
odd-tortoise <126099976+odd-tortoise@users.noreply.github.com>
2024-06-05 13:38:34 +0200

    Update job.sh

--- job.sh
+++ job.sh
@@ -14,4 +14,4 @@ SINGULARITY_IMAGE=my_container.sif
 export TMPDIR=$HOME/tmp
 mkdir -p $TMPDIR
 
-singularity exec --bind $TMPDIR:$TMPDIR $SINGULARITY_IMAGE bash -c "export OMPI_MCA_tmpdir_base=$TMPDIR && mpirun -np 2 build/src/main"
+singularity exec --bind $TMPDIR:$TMPDIR $SINGULARITY_IMAGE bash -c "export OMPI_MCA_tmpdir_base=$TMPDIR && mpirun -np 2 /project/main"
