4b5675b140857639fa34e48c0d82f1c372975f7c
odd-tortoise <126099976+odd-tortoise@users.noreply.github.com>
2024-06-05 12:37:17 +0200

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-SING_IMG = my_container.sif
-
-export TMPDIR = $HOME/tmp
-mkdir -p $TMPDIR
-
-singularity exec --bind $TMPDIR:$TMPDIR $SING_IMG
+singularity exec --bind my_container.sif
