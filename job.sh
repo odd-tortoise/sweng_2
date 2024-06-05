84a8d10018735e7139b74e32c792f81256ba8954
odd-tortoise <126099976+odd-tortoise@users.noreply.github.com>
2024-06-05 13:08:06 +0200

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+#SBATCH --job-name=matrix_multiplication
+#SBATCH --output=output.txt
+#SBATCH --error=error.txt
+#SBATCH --ntasks=2
+#SBATCH --nodes=1
+#SBATCH --time=00:10:00
+#SBATCH --partition=g100_all_serial
 
-singularity run my_container.sif
+module load singularity
+
+SINGULARITY_IMAGE=my_container.sif
+
+export TMPDIR=$HOME/tmp
+mkdir -p $TMPDIR
+
+singularity exec --bind $TMPDIR:$TMPDIR $SINGULARITY_IMAGE bash -c "export OMPI_MCA_tmpdir_base=$TMPDIR && mpirun -np 2 /src/main"
